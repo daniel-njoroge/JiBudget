d2e8e29fd55e8805e25de353c34eff898452803b
Daniel Njoroge <njorogedaniel088@gmail.com>
2025-04-01 22:54:24 +0300

    layout2

--- app/src/main/java/com/example/jibudget/MainActivity.kt
+++ app/src/main/java/com/example/jibudget/MainActivity.kt
@@ -6,22 +6,25 @@ import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import com.example.jibudget.ui.theme.JibudgetTheme
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Box
+import androidx.compose.ui.layout.ContentScale
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -33,34 +36,53 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
-                    GreetingText(message = "Welcome to JiBudget!",
-                        from = "From Njoroge",
-                        modifier = Modifier.padding(8.dp))
+                    GreetingImage(message = "Welcome to JiBudget!",
+                        from = "From Njoroge")
 
                 }
             }
         }
     }
 }
+
+
+@Composable
+fun GreetingImage(message: String, from: String, modifier: Modifier = Modifier) {
+    val image = painterResource(R.drawable.androidparty)
+    Box(modifier) {
+        Image(
+            painter = image,
+            contentDescription = null,
+            contentScale = ContentScale.Crop,
+            alpha = 0.5F
+        )
+        GreetingText(
+            message = "Welcome to JiBudget!",
+            from = "From Njoroge",
+            modifier = Modifier
+                .fillMaxSize()
+                .padding(8.dp))
+    }
+}
+
 @Composable
 fun GreetingText(message: String,from: String,modifier: Modifier = Modifier) {
     Column(
-        verticalArrangement = Arrangement.Center,
+        verticalArrangement = Arrangement.Top,
+        horizontalAlignment = Alignment.CenterHorizontally,
         modifier = modifier
         ) {
         Text(
             text = message,
             fontSize = 30.sp,
             lineHeight = 70.sp,
-            textAlign = TextAlign.Center
+            textAlign = TextAlign.Center,
+            modifier = Modifier.background(color = Color.LightGray)
 
         )
         Text(
             text = from,
-            fontSize = 36.sp,
-            modifier = Modifier
-                .padding(16.dp)
-                .align(alignment = Alignment.End)
+            fontSize = 36.sp
 
         )
     }
@@ -70,6 +92,6 @@ fun GreetingText(message: String,from: String,modifier: Modifier = Modifier) {
 @Composable
 fun GreetingPreview() {
     JibudgetTheme {
-        GreetingText(message = "Welcome to JiBudget!",from = "From Njoroge")
+        GreetingImage(message = "Welcome to JiBudget!",from = "From Njoroge")
     }
 }
